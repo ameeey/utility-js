1a42c60decbb7d3f0721d83ba9acafe542c70314
Amey Dipak Gulhane <41951939+ameeey@users.noreply.github.com>
2021-03-04 15:39:14 +0530

    min function added with testcase

---
+++ src/min.spec.js
@@ -0,0 +1,13 @@
+const min=require('./min');
+
+describe('Min', () => {
+
+	it('Min of [] is undefined', () => {
+        expect(min([])).toEqual(undefined);
+    });
+
+    it('Min of [1,2,3,4] is 4', () => {
+        expect(min([1,2,3,4])).toEqual(1);
+    });
+
+})
