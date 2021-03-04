f48a293f384ab4849c805893f9868f8bc064c74b
Amey Dipak Gulhane <41951939+ameeey@users.noreply.github.com>
2021-03-04 15:54:12 +0530

    tail function added with testcases

--- src/tail.spec.js
+++ src/tail.spec.js
@@ -2,7 +2,7 @@ const tail=require('./tail');
 
 describe('Tail',() => {
     it('Tail of [1,2,3] is [2,3]',() => {
-        expect(tail([1,2,3])).toEqual(1);
+        expect(tail([1,2,3])).toEqual([2,3]);
     });
 
     it('Tail of [] is undefined',() => {
